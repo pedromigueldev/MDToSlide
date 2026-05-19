8a4151c2e7e58ce130d193f17e09b71771414271
pedromigueldev <pedro.miguel.e.idk@gmail.com>
2026-05-19 00:06:58 -0300

    feat: ul block and blockquote are handled

--- main.c
+++ main.c
@@ -25,6 +25,7 @@ int main(int argc, char** argv) {
 	};
 
 	Mstr* token = NULL;
+	bool inBody = false;
 	for(;;) {
 		catch(mstr_chop_by, &token, file_buffer, '\n') {
 			fprintf(stderr, "Failed to chop md: %s\n", unwrap_fail(token));
@@ -38,12 +39,27 @@ int main(int argc, char** argv) {
 		        Mstr* content = NULL;
 				mstr_chop_by_tryfail(&content, token, '#');
 				MStringPoolPush(&html, MPRINT_FMT_OUT("<h1>"MFMT(token)"</h1>"));
+				free(content);
 		        break;
 		    case '-':
+		    	Mstr* li = NULL;
+				mstr_chop_by_tryfail(&li, token, '-');
 				int count = 0;
 				while (token->raw[count] == '-') count++;
 				if (count == 3)
 					MStringPoolPush(&html, MPRINT_FMT_OUT("<hr>"));
+				if (count < 2) {
+					if (inBody) {
+						MStringPoolPush(&html, MPRINT_FMT_OUT("<li>"MFMT(token)"</li>"));
+						continue;
+					} else {
+						inBody = true;
+						MStringPoolPush(&html, MPRINT_FMT_OUT("<ul>"));
+						MStringPoolPush(&html, MPRINT_FMT_OUT("<li>"MFMT(token)"</li>"));
+						continue;
+					}	
+				}
+				free(li);
 		        break;
 		    case '!':
 		    	Mstr* gbg = NULL;
@@ -54,6 +70,9 @@ int main(int argc, char** argv) {
    				mstr_chop_by_tryfail(&gbg, token, '(');
  				mstr_chop_by_tryfail(&image_url, token, ')');
    				MStringPoolPush(&html, MPRINT_FMT_OUT("<img alt='"MFMT(image_alt)"' src='"MFMT(image_url)"'/>"));
+   				free(gbg);
+   				free(image_alt);
+   				free(image_url);
    		        break;
    		    case '*':
 		    	Mstr* gbgb = NULL;
@@ -65,27 +84,47 @@ int main(int argc, char** argv) {
 				catch(mstr_chop_by, &gbgb, token, '*') break;
 				catch(mstr_chop_by, &gbgb, token, '*') break;
 				MStringPoolPush(&html, MPRINT_FMT_OUT("<b>"MFMT(bold_content)"</b>"));
+				free(gbgb);
+				free(bold_content);
   		        break;
+  		    case '>':
+  		    	Mstr* blockquote = NULL;
+  		    	mstr_chop_by_tryfail(&blockquote, token, '>');
+		    	MStringPoolPush(&html, MPRINT_FMT_OUT("<blockquote><p>"MFMT(token)"</p></blockquote>"));
+		    	free(blockquote);
+   		        break;
 		    default:
 		        MStringPoolPush(&html, MPRINT_FMT_OUT("<p>"MFMT(token)"</p>"));
 		}
+		
+	    if (inBody) {
+	    	char * temp = MVecGet(html, (MVecLen(html) - 1));
+    		MVecSet(html, (MVecLen(html) - 1), MPRINT_FMT_OUT("</ul>"));
+    		MStringPoolPush(&html, temp);
+    	 	inBody = false;
+	    };
+	    free(token);
 	}
 
 	char* out = NULL;
 	char* path = MPRINT_FMT_OUT("./"$(fileName)".html");
 	char* htmlContent = NULL;
+
 	MVecForeach(item, html) {
+		MPRINT_FMT($(item));
 		htmlContent = MPRINT_FMT_OUT($(htmlContent ? htmlContent : "")$(item));
 		free(item);
+		free(htmlContent);
 	}
+	
 	catch(mfile_create, &out, path, htmlContent) {
 		fprintf(stderr, "Error wrinting html: %s\n", unwrap_fail(out));
 		free(out);
 		return 1;
 	};
 
-	free(MVec(html));
 	free(path);
+	free(MVec(html));
 	free(file_buffer);
 	return 0;				
 }							
