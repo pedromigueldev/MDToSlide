3d39d4a55d1c69d6278641638fdcb07e1cfdbe4a
pedromigueldev <pedro.miguel.e.idk@gmail.com>
2026-05-21 12:51:32 -0300

    update: mlib vendor

--- main.c
+++ main.c
@@ -6,17 +6,17 @@
 
 static char tokens[] = { '#', '*', '-', '.', '|', '>'};
 int main(int argc, char** argv) {
-	MVecAlloc(pool, char, 500);
-	MVecAlloc(html, char, 500);
+	__free(strfree) MVecAlloc(pool, char, 500);
+	__free(strfree) MVecAlloc(html, char, 500);
 	if (argc < 2 || argv[1] == NULL) {
-		fprintf(stderr, "provdie a markdown\n");
+		MPrintFmt("provdie a markdown");
 		return 1;
 	}
 	
 	MstrView fileName = MStrFmt(&pool, $(argv[1]));
 	MRetEither(mk, mkerr, mfile_read(MVecParamRefPtr(&pool), fileName));
 	if (mkerr) {
-		MPrintFmt($(mkerr));
+		MPrintFmt("File read fail: "$(mkerr));
 		return 1;
 	}
 
@@ -87,113 +87,8 @@ int main(int argc, char** argv) {
 	MstrView filen = MStrFmt(&pool, MstrViewFmt(fileName)".html");
 	MRetEither(out, err, mfile_create(filen, content));
 	if (err) {
-		MPrintFmt("mfile_create: %s", err);
+		MPrintFmt("mfile_create: %s", strerror(err));
 	}
 	
-	free(MVec(pool));
-	free(MVec(html));
 	return 0;
-// 	Mstr* token = NULL;
-// 	bool inBody = false;
-// 	for(;;) {
-// 		catch(mstr_chop_by, &token, file_buffer, '\n') {
-// 			fprintf(stderr, "Failed to chop md: %s\n", unwrap_fail(token));
-// 			return 1;
-// 		}
-// 		if (token == NULL) break;
-// 		mstr_trim(token);
-// 		if (token->length == 0) continue;
-// 		switch (token->raw[0]) {
-// 		    case '#':
-// 		        Mstr* content = NULL;
-// 				mstr_chop_by_tryfail(&content, token, '#');
-// 				MStringPoolPush(&html, MPRINT_FMT_OUT("<h1>"MFMT(token)"</h1>"));
-// 				free(content);
-// 		        break;
-// 		    case '-':
-// 		    	Mstr* li = NULL;
-// 				mstr_chop_by_tryfail(&li, token, '-');
-// 				int count = 0;
-// 				while (token->raw[count] == '-') count++;
-// 				if (count == 3)
-// 					MStringPoolPush(&html, MPRINT_FMT_OUT("<hr>"));
-// 				if (count < 2) {
-// 					if (inBody) {
-// 						MStringPoolPush(&html, MPRINT_FMT_OUT("<li>"MFMT(token)"</li>"));
-// 						continue;
-// 					} else {
-// 						inBody = true;
-// 						MStringPoolPush(&html, MPRINT_FMT_OUT("<ul>"));
-// 						MStringPoolPush(&html, MPRINT_FMT_OUT("<li>"MFMT(token)"</li>"));
-// 						continue;
-// 					}	
-// 				}
-// 				free(li);
-// 		        break;
-// 		    case '!':
-// 		    	Mstr* gbg = NULL;
-// 		    	Mstr* image_alt = NULL;
-// 		    	Mstr* image_url = NULL;
-//    				mstr_chop_by_tryfail(&gbg, token, '[');
-//    				mstr_chop_by_tryfail(&image_alt, token, ']');
-//    				mstr_chop_by_tryfail(&gbg, token, '(');
-//  				mstr_chop_by_tryfail(&image_url, token, ')');
-//    				MStringPoolPush(&html, MPRINT_FMT_OUT("<img alt='"MFMT(image_alt)"' src='"MFMT(image_url)"'/>"));
-//    				free(gbg);
-//    				free(image_alt);
-//    				free(image_url);
-//    		        break;
-//    		    case '*':
-// 		    	Mstr* gbgb = NULL;
-// 		    	Mstr* bold_content = NULL;
-//    		    	
-// 	    		mstr_chop_by_tryfail(&gbgb, token, '*');
-// 	    		mstr_chop_by_tryfail(&gbgb, token, '*');
-// 				catch(mstr_chop_by, &bold_content, token, '*') break;
-// 				catch(mstr_chop_by, &gbgb, token, '*') break;
-// 				catch(mstr_chop_by, &gbgb, token, '*') break;
-// 				MStringPoolPush(&html, MPRINT_FMT_OUT("<b>"MFMT(bold_content)"</b>"));
-// 				free(gbgb);
-// 				free(bold_content);
-//   		        break;
-//   		    case '>':
-//   		    	Mstr* blockquote = NULL;
-//   		    	mstr_chop_by_tryfail(&blockquote, token, '>');
-// 		    	MStringPoolPush(&html, MPRINT_FMT_OUT("<blockquote><p>"MFMT(token)"</p></blockquote>"));
-// 		    	free(blockquote);
-//    		        break;
-// 		    default:
-// 		        MStringPoolPush(&html, MPRINT_FMT_OUT("<p>"MFMT(token)"</p>"));
-// 		}
-// 		
-// 	    if (inBody) {
-// 	    	char * temp = MVecGet(html, (MVecLen(html) - 1));
-//     		MVecSet(html, (MVecLen(html) - 1), MPRINT_FMT_OUT("</ul>"));
-//     		MStringPoolPush(&html, temp);
-//     	 	inBody = false;
-// 	    };
-// 	    free(token);
-// 	}
-// 
-// 	char* out = NULL;
-// 	char* path = MPRINT_FMT_OUT("./"$(fileName)".html");
-// 	char* htmlContent = NULL;
-// 
-// 	MVecForeach(item, html) {
-// 		MPRINT_FMT($(item));
-// 		htmlContent = MPRINT_FMT_OUT($(htmlContent ? htmlContent : "")$(item));
-// 		free(item);
-// 		free(htmlContent);
-// 	}
-// 	
-// 	catch(mfile_create, &out, path, htmlContent) {
-// 		fprintf(stderr, "Error wrinting html: %s\n", unwrap_fail(out));
-// 		free(out);
-// 		return 1;
-// 	};
-// 
-// 	free(path);
-// 	free(MVec(html));
-// 	free(file_buffer);
-// 	return 0;				
 }							
